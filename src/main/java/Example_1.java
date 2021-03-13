67e4825a9c6e629a369dfdd4750c4c4079616139
megavork <megavork@mail.ru>
2021-03-14 00:26:36 +0300

    Please, check maven (pom file).

---
+++ src/main/java/Example_1.java
@@ -0,0 +1,13 @@
+
+public class Example_1 {
+
+	public static void main(String[] args) {
+		System.out.println(equalsOfTwoIntegers(2,3));
+
+	}
+	
+	public static boolean equalsOfTwoIntegers(int one, int two) {
+		return one==two;
+	}
+
+}
---
+++ src/test/java/Example_1_test.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class Example_1_test {
+	@Test
+	public void sumOfTwoIntegersAble() {
+		assertEquals(true, Example_1.equalsOfTwoIntegers(2, 3));
+	}
+}
